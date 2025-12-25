87b2c209be767e21622e242686f19bf3eee90c06
LombreRode <starter84@hotmail.fr>
2025-12-25 21:13:40 +0100

    Update user_sync.js

--- auth/user_sync.js
+++ auth/user_sync.js
@@ -1,6 +1,4 @@
 // auth/user_sync.js
-// Crée/MAJ un document Firestore /users/{uid} dès qu'un user est connecté
-
 import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
@@ -8,19 +6,16 @@ import { doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs
 onAuthStateChanged(auth, async (user) => {
   if (!user) return;
 
-  try {
-    await setDoc(
-      doc(db, "users", user.uid),
-      {
-        uid: user.uid,
-        email: user.email || null,
-        displayName: user.displayName || null,
-        lastLoginAt: serverTimestamp(),
-      },
-      { merge: true }
-    );
-  } catch (e) {
-    // silencieux pour ne pas casser le site
-    console.error("user_sync error:", e);
-  }
+  // IMPORTANT: on ne touche JAMAIS à "role" ici.
+  // Et on MERGE pour ne pas écraser les champs existants.
+  await setDoc(
+    doc(db, "users", user.uid),
+    {
+      uid: user.uid,
+      email: user.email || null,
+      displayName: user.displayName || null,
+      updatedAt: serverTimestamp(),
+    },
+    { merge: true }
+  );
 });
