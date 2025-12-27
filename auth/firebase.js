92b57c237e581a89afed928ea5609cc2414a88cd
LombreRode <starter84@hotmail.fr>
2025-12-27 20:00:56 +0100

    Update firebase.js

--- auth/firebase.js
+++ auth/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "XXXX",
-  authDomain: "XXXX.firebaseapp.com",
-  projectId: "XXXX",
-  storageBucket: "XXXX.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyA6GuK8h2YGbWARC8ybLQa5L06ZcdePM",
+  authDomain: "wauklink-9b61f.firebaseapp.com",
+  databaseURL: "https://wauklink-9b61f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "wauklink-9b61f",
+  storageBucket: "wauklink-9b61f.firebasestorage.app",
+  messagingSenderId: "398288789090",
+  appId: "1:398288789090:web:82b7c1bcbd4ad47af1fb3b"
 };
 
 const app = initializeApp(firebaseConfig);
