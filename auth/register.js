5f01f68e33086555b5bd5fd133f11540a6fbe015
LombreRode <starter84@hotmail.fr>
2025-12-28 19:08:45 +0100

    Update register.js

--- auth/register.js
+++ auth/register.js
@@ -1,19 +1,16 @@
-// auth/register.js
-import { auth } from "../_shared/firebase.js";
+import { auth } from "../shared/firebase.js";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const form = document.getElementById("registerForm");
-const msg  = document.getElementById("msg");
+const msg = document.getElementById("msg");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    window.location.href = "../index.html";
-  }
+  if (user) window.location.href = "../index.html";
 });
 
 form.addEventListener("submit", async (e) => {
@@ -26,10 +23,8 @@ form.addEventListener("submit", async (e) => {
       email.value.trim(),
       password.value.trim()
     );
-
     window.location.href = "../index.html";
   } catch (err) {
-    console.error(err);
-    msg.textContent = "❌ " + (err.message || "Erreur d’inscription");
+    msg.textContent = "❌ " + err.message;
   }
 });
