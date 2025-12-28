c38d5c268717220f703b81632e188144e8ea6921
LombreRode <starter84@hotmail.fr>
2025-12-28 20:15:24 +0100

    Update firebase.js

--- _shared /firebase.js
+++ _shared /firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+// _shared/firebase.js
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
@@ -11,7 +12,9 @@ const firebaseConfig = {
   appId: "XXX"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
