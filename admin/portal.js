8fd34ae0cccaf05ad9060bf96cf86673c4d2763e
LombreRode <starter84@hotmail.fr>
2025-12-29 16:29:48 +0100

    Update portal.js

--- admin/portal.js
+++ admin/portal.js
@@ -1,17 +1,6 @@
-// admin/portal.js
 import { requireModeration } from "../_shared/guard.js";
-
-const msg = document.getElementById("msg");
-
-msg.textContent = "Vérification des droits…";
 
 requireModeration({
   redirectTo: "../auth/login.html",
-  onOk: (user) => {
-    msg.textContent = "Accès autorisé";
-
-    // Ici tu peux rediriger selon ton besoin
-    // Exemple simple :
-    location.replace("./index.html");
-  }
+  onOk: () => location.replace("./index.html")
 });
