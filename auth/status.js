c8e5851da47310df960642f249e2f3f4735129f6
LombreRode <starter84@hotmail.fr>
2025-12-30 20:57:13 +0100

    Update status.js

--- auth/status.js
+++ auth/status.js
@@ -1,19 +1,13 @@
 // auth/status.js
-import { auth } from "/wauklink-site/shared/firebase.js";
+import { auth } from "../shared/firebase.js";
+
 import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-// ⚠️ Script informatif uniquement
-// ❌ Pas de redirection
-// ❌ Pas de Firestore
-// ❌ Pas de rôle
-
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // utilisateur connecté
     document.body.classList.add("user-logged");
   } else {
-    // visiteur
     document.body.classList.remove("user-logged");
   }
 });
