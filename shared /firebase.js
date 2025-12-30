824f7204c57534c318c669ddf0462af33d5b70d2
LombreRode <starter84@hotmail.fr>
2025-12-30 10:18:51 +0100

    Update firebase.js

--- shared /firebase.js
+++ shared /firebase.js
@@ -1,8 +1,9 @@
-alert("🔥 FIREBASE.JS CHARGÉ 🔥");
-
-import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { initializeApp, getApps, getApp } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "XXX",
