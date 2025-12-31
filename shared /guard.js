8b1fad5403b1cd49ac173ecb408c3f62f6406ad4
LombreRode <starter84@hotmail.fr>
2026-01-01 00:17:13 +0100

    Update guard.js

--- shared /guard.js
+++ shared /guard.js
@@ -1,5 +1,5 @@
 // shared/guard.js
-import { auth, db } from "./firebase.js";
+import { auth, db } from "../shared/firebase.js";
 import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { doc, getDoc } from
