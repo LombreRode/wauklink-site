930e92cc4b27c4a1b76076bb085629bb7c3b5668
LombreRode <starter84@hotmail.fr>
2026-01-01 13:30:24 +0100

    Create firebase.js

---
+++ shared/firebase.js
@@ -0,0 +1,25 @@
+// shared/firebase.js
+import { initializeApp, getApps, getApp } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX.firebaseapp.com",
+  projectId: "XXX",
+  storageBucket: "XXX.appspot.com",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+};
+
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
+
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
