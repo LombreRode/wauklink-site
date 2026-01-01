9304cc34206a3f4c828bf7aebdd59056cc6a1187
LombreRode <starter84@hotmail.fr>
2026-01-01 11:58:49 +0100

    Update firebase.js

--- shared /firebase.js
+++ shared /firebase.js
@@ -1,10 +1,13 @@
 // shared/firebase.js
 import { initializeApp, getApps, getApp } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
 import { getAuth } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
 import { getFirestore } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
 import { getStorage } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
