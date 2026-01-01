901fdb59b96abccd174d1339e92ef56462a02854
LombreRode <starter84@hotmail.fr>
2026-01-01 12:26:21 +0100

    Update status.js

--- auth/status.js
+++ auth/status.js
@@ -1,15 +1,13 @@
 // auth/status.js
 import { auth } from "../shared/firebase.js";
-
 import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
 onAuthStateChanged(auth, (user) => {
-  
   if (user) {
     document.body.classList.add("user-logged");
     window.isAuthenticated = true;
     window.authUser = user;
-    
   } else {
     document.body.classList.remove("user-logged");
     window.isAuthenticated = false;
