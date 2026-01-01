51cfd38ff2d6fb2b099beaa29df6d866df39e697
LombreRode <starter84@hotmail.fr>
2026-01-01 16:47:58 +0100

    Update portal.js

--- admin/portal.js
+++ admin/portal.js
@@ -1,11 +1,12 @@
 // admin/portal.js
-import { auth } from "../shared/firebase.js";
+import { auth } from "/wauklink-site/shared/firebase.js";
+import { requireAdmin } from "/wauklink-site/shared/guard.js";
 
 const msg = document.getElementById("msg");
 
 requireAdmin({
   redirectTo: "/wauklink-site/auth/login.html",
   onOk: () => {
-    msg.textContent = "Accès admin autorisé";
+    msg.textContent = "✅ Accès admin autorisé";
   }
 });
