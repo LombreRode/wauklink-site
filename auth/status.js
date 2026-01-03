d09f329b1f864f717f590a1226e8f9094ec8d553
LombreRode <starter84@hotmail.fr>
2026-01-03 21:34:42 +0100

    Update status.js

--- auth/status.js
+++ auth/status.js
@@ -1,16 +1,17 @@
 // auth/status.js
 import { auth } from "/wauklink-site/shared/firebase.js";
-import { onAuthStateChanged } from
-  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    document.body.classList.add("user-logged");
-    window.isAuthenticated = true;
-    window.authUser = user;
-  } else {
-    document.body.classList.remove("user-logged");
-    window.isAuthenticated = false;
-    window.authUser = null;
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      document.body.classList.add("user-logged");
+      window.isAuthenticated = true;
+    } else {
+      document.body.classList.remove("user-logged");
+      window.isAuthenticated = false;
+    }
+  });
 });
