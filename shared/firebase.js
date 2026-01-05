95f4ecfeb4b0fced50b075d2f53585782153cd20
LombreRode <starter84@hotmail.fr>
2026-01-05 07:33:37 +0100

    Create firebase.js

---
+++ shared/firebase.js
@@ -0,0 +1,39 @@
+// =================================================
+// WAUKLINK — Firebase central
+// Compatible GitHub Pages
+// Version stable
+// =================================================
+
+import { initializeApp, getApps, getApp } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+// =========================
+// CONFIG FIREBASE
+// =========================
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "wauklink-9b61f.firebaseapp.com",
+  projectId: "wauklink-9b61f",
+  storageBucket: "wauklink-9b61f.appspot.com",
+  messagingSenderId: "398288789090",
+  appId: "1:398288789090:web:82b7c1cbcd4ad47af1fb3b"
+};
+
+// =========================
+// INIT UNIQUE (ANTI DOUBLE INIT)
+// =========================
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
+
+// =========================
+// EXPORTS
+// =========================
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
