c4ce37d95cbd7f780693f38fe579a49b38bc87f6
LombreRode <starter84@hotmail.fr>
2026-01-06 17:23:59 +0100

    Create pricing-auth.js

---
+++ shared/pricing-auth.js
@@ -0,0 +1,24 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+const loginBtn = document.querySelector(".btn-login");
+const signupLinks = document.querySelectorAll(".signup-link");
+const dashboardBtn = document.querySelector(".btn-dashboard");
+const logoutBtn = document.querySelector(".btn-logout");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // 🔒 Utilisateur connecté
+    loginBtn?.classList.add("hidden");
+    signupLinks.forEach(el => el.classList.add("hidden"));
+    dashboardBtn?.classList.remove("hidden");
+    logoutBtn?.classList.remove("hidden");
+  } else {
+    // 👤 Visiteur
+    loginBtn?.classList.remove("hidden");
+    signupLinks.forEach(el => el.classList.remove("hidden"));
+    dashboardBtn?.classList.add("hidden");
+    logoutBtn?.classList.add("hidden");
+  }
+});
